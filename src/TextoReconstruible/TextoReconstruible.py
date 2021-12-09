9cfe1aff3c1b330d2cacfc43c7218f3ba9323598
ElReyZero <juan.romero1201@gmail.com>
2021-12-09 16:16:34 -0500

    Ejercicio 3 casi completado

--- src/TextoReconstruible/TextoReconstruible.py
+++ src/TextoReconstruible/TextoReconstruible.py
@@ -1,4 +1,55 @@
+import time
+"""
+Autores:
+Juan Andrés Romero Colmenares - 202013449
+Luccas Rojas - 201923052
+"""
+def textoReconstruible(lista:list[str])->str:
+    aux = []
+    aux2 = []
+    auxi=""
+    auxj=""
+    auxStr=""
+    for i in range(len(lista)):
+        aux.append(lista[i])
+    cuenta = -1
+    while len(aux) != 1:
+        for i in range(0, len(aux)):
+            for j in range(i+1, len(aux)):
+                numOverlap, strOverlap = overlap(aux[i], aux[j])
+                if numOverlap>cuenta:
+                    cuenta =numOverlap
+                    auxStr = strOverlap
+                    auxj= aux[j]
+                    auxi= aux[i]
+                elif numOverlap == cuenta and strOverlap not in aux2 and not isBlank(strOverlap):
+                    aux2.append(strOverlap)
 
+
+        if not isBlank(auxi):
+            aux.remove(auxi)
+        if not isBlank(auxj):
+            aux.remove(auxj)
+        if not isBlank(auxStr):
+            aux.append(auxStr)
+
+        if len(aux) == 0 and len(aux2) != 0:
+            aux.extend(aux2)
+            aux2 = []
+            for string in lista:
+                if not any(string in elemento for elemento in aux):
+                    aux.append(string)
+        elif len(aux2) == 0 and numOverlap == 0:
+            res = ""
+            for string in lista:
+                res += string
+            return res 
+        auxStr=""
+        auxi=""
+        auxj=""
+        cuenta = -1
+    
+    return aux[0]
 
 def overlap(a:str,b:str)-> tuple:
     max = 0
@@ -17,38 +68,6 @@ def overlap(a:str,b:str)-> tuple:
                 max =i
                 strRes = b + a[i:]
     return max, strRes
-
-
-def textoReconstruible(lista:list[str], k:int)->str:
-    aux = []
-    auxi=""
-    auxj=""
-    auxStr=""
-    for i in range(len(lista)):
-        aux.append(lista[i])
-    cuenta = -1
-    while len(aux) != 1:
-        for i in range(0, len(aux)):
-            for j in range(i+1, len(aux)):
-                numOverlap, strOverlap = overlap(aux[i], aux[j])
-                if numOverlap>cuenta:
-                    cuenta =numOverlap
-                    auxStr = strOverlap
-                    auxj= aux[j]
-                    auxi= aux[i]
-        
-        if not isBlank(auxi):
-            aux.remove(auxi)
-        if not isBlank(auxj):
-            aux.remove(auxj)
-        if not isBlank(auxStr):
-            aux.append(auxStr)
-        auxStr=""
-        auxi=""
-        auxj=""
-        cuenta = -1
-    
-    return aux[0]
             
 
 def isBlank(cadena:str)->bool:
@@ -57,9 +76,15 @@ def isBlank(cadena:str)->bool:
     else:
         return False
 
-
 def main():
-    print(textoReconstruible(["aab","baa","aaa","bbb"], 3))
-
+    start_time = time.time()
+    #print(textoReconstruible(["RRRRRRRRRRRR", "RRRRRRRRRRRR", "RRRRRRRRRRRR", "RRRRRRRRRRRR", "RRRRRRRRRRRR", "RRRRRRRRRRRR"]))
+    #print(textoReconstruible(["nfid", "conf", "cial", "denc", "onfi", "enci"]))
+    #print(textoReconstruible(["aab","baa","aaa","bbb"]))
+    #print(textoReconstruible(["abb","bbc","bbb"]))
+    #print(textoReconstruible(["alex","loves","leetcode"]))
+    #print(textoReconstruible(["catg","ctaagt","gcta","ttca","atgcatc"]))
+    print(textoReconstruible(["efde","defab", "abcdef"]))
+    print("%s segundos" % (time.time()-start_time))
 if __name__ == '__main__':
     main()
